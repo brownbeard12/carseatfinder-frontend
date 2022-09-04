706195d500aab3b8e3845daeecef65c9e644befe
brownbeard12 <31408027+brownbeard12@users.noreply.github.com>
2022-09-03 21:58:55 -0400

    format

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(config) {
 
     config.addPassthroughCopy('src/css/main.css');
     config.addPassthroughCopy('src/js/index.js');
+
     return {
         passthroughFileCopy: true,
         dir: {
